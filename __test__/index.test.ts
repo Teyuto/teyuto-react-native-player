f1123fa4c138bd949565725159517cfbb0566b85
marcellov7 <104211738+marcellov7@users.noreply.github.com>
2023-11-14 18:35:59 +0100

    fix test

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -6,6 +6,6 @@ const getEmbedUrlForConfig = (config: PlayerProps) => new TeyutoPlayerSdk(config
 describe('Embed URL are properly generated', () => {
     it('generate simple urls', () => {
         expect(getEmbedUrlForConfig({id: "123456"}))
-            .to.equal("https://teyuto.tv/video/player?w=123456");
+            .to.equal("https://teyuto.tv/video/player?w=123456&pip=off");
     });
 });
